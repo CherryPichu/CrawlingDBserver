a2cb6da78791d1d09077ca7ad802e48f1ce01e8e
CherryPichu <uskawjdu@gmail.com>
2024-05-07 00:32:40 +0900

    init

---
+++ run_script3.sh
@@ -0,0 +1,6 @@
+# OST.py 실행
+script_name="./script/HNJ.py"
+# 이미 실행 중인지 확인 후, 실행 중이면 종료
+pkill -f $script_name
+# 스크립트 실행
+python3 ~/ubuntu_shared_folder/GIT/CrawlingWeb/$script_name>> ~/ubuntu_shared_folder/GIT/CrawlingWeb/test3.txt
