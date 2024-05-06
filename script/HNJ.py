41bc6f25efdeb1c7b612182ebfcf9dc9c1debd84
CherryPichu <uskawjdu@gmail.com>
2024-05-06 23:57:23 +0900

    init

--- script/HNJ.py
+++ script/HNJ.py
@@ -5,7 +5,7 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import time
-
+from urllib.parse import urlparse, parse_qs
 class Graph :
     def __init__(self) :
         self.graph = {}
@@ -21,14 +21,46 @@ class Graph :
             for v in self.graph[u] :
                 print(v, end=" ")
             print()
-
-graph = Graph()
+UNIQUENAME = "NAMJUNG"
 proxies = {
-     'http' : "socks5h://uskawjdu.iptime.org:9052",
-     'https' : "socks5h://uskawjdu.iptime.org:9052"
+    'http' : "socks5h://uskawjdu.iptime.org:9052",
+    'https' : "socks5h://uskawjdu.iptime.org:9052"
 }         
 
+def main() :
+    
+    server_path = "http://uskawjdu.iptime.org:8001/getUrl?name=%s" %(UNIQUENAME)
+    origin_url = requests.get(server_path)
+    if origin_url is None:
+        return
+    origin_url_json = origin_url.json()
+    
+    target = origin_url_json["url"]
+    depth = origin_url_json["Depth"]
+
+    
+    
+    
+    onion_link = target
+    visit_onion(onion_link, 3)
+
+
+    dot = pydot.Dot( graph_type="digraph", rankdir="LR")
+
+    # 그래프 생성
+    for (u, vList) in graph.graph.items() :
+        if u == "http://4t4ki52bkw46s6zfxnnlcnzrqjv4zdp5kwmqeqtouwbixuxp5kfcxyad.onion" :
+            pydot.Node(u, fillcolor='#FF0000')
+        else : 
+            pydot.Node(u)
+        for v in vList :
+            edge = pydot.Edge(u, v, arrowhead='vee')
+            dot.add_edge(edge)
+    dot.write_png('darkWeb.png')
+    
+graph = Graph()
 visitedList = set()
+
 def visit_onion(onion_link, depth : int):
     onion_link = onion_link.replace("#", "")
     onion_link = onion_link.replace("../", "/")
@@ -51,6 +83,27 @@ def visit_onion(onion_link, depth : int):
     
     soup = BeautifulSoup(response.text, "html.parser")
     title = soup.title.__str__().replace("<title>", "").replace("</title>", "")
+    parsed_url = urlparse(url)
+    data = {
+            "name" : UNIQUENAME,
+            "origin_url": onion_link,
+            "parameter":  parse_qs(parsed_url.query),
+            "title": title,
+            "url":  parsed_url.__str__(),
+            "domain": parsed_url.netloc,
+            "HTML": response.text,
+            "wordlist" : json.dumps(response.text.split(" ")),
+            "referer" : ""
+    }
+    # print(data)
+    try:
+        response = requests.post("http://uskawjdu.iptime.org:8001/postData", json=data)
+        response.close()
+        print(f"requests 에러 발생: {e}, http://uskawjdu.iptime.org:8001/postData")
+    except Exception as e:
+        print("error url : ", child_url)
+    
+    graph.addEdge(onion_link, href)
     
     atags = soup.find_all("a")
     hrefs = []
@@ -75,30 +128,13 @@ def visit_onion(onion_link, depth : int):
         else :
             href = onion_link + "/" + href
             
-  
-            
-        graph.addEdge(onion_link, href)
+        
+        
+        
         visit_onion(href, depth - 1)
         
-            
-        
 
-
-onion_link = "http://4t4ki52bkw46s6zfxnnlcnzrqjv4zdp5kwmqeqtouwbixuxp5kfcxyad.onion"
-visit_onion(onion_link, 3)
-
-
-dot = pydot.Dot( graph_type="digraph", rankdir="LR")
-
-# 그래프 생성
-for (u, vList) in graph.graph.items() :
-    if u == "http://4t4ki52bkw46s6zfxnnlcnzrqjv4zdp5kwmqeqtouwbixuxp5kfcxyad.onion" :
-        pydot.Node(u, fillcolor='#FF0000')
-    else : 
-        pydot.Node(u)
-    for v in vList :
-        edge = pydot.Edge(u, v, arrowhead='vee')
-        dot.add_edge(edge)
-dot.write_png('darkWeb.png')
+if __name__ == "__main__":
+    main()
 
 
